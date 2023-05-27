676b86483bfb28e68dd284227faa70af19b5aa86
Abhishek Singhal <abhisinghal112001@gmail.com>
2023-05-27 22:47:56 +0530

    calendar

--- app/page.js
+++ app/page.js
@@ -1,15 +1,13 @@
 import {assignment} from '@/Data/Data'
 import Assignment from '@/components/Assignment'
+import Calender from '@/components/Calender'
 import Card1 from '@/components/Card1'
-import Image from 'next/image'
 
 
 export default function Home() {
   return (
     <div className='center h-screen w- screen'>
-      <Assignment
-        assignment={assignment}
-      />
+      <Calender/>
     </div>
   )
 }
---
+++ components/Calender.js
@@ -0,0 +1,49 @@
+
+import React from 'react'
+import {AiOutlineRight} from "react-icons/ai"
+
+const Calender = () => {
+    const week = ["Sun","Mon","Tues","Wed","Thrus","Fri","Sat"]
+    const title = ["Team Meetup","Retrospective","","illustration","","Traning",""]
+    const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    const next7 = []
+    for(let i = 0;i<7;i++){
+        next7.push(new Date(new Date().setDate(new Date().getDate() + i)))
+    }
+  return (
+    <div className='component w-[350px] space-y-4'>
+        {
+            next7.map((e,i)=>(
+                <div className='center space-x-5 mb-3 w-full whitespace-nowrap' key={i}>
+                    
+            {/* date */}
+            <div className='w-[70px] '>
+                
+                <h3 className='capitalize  relative'> {i==0 && <AiOutlineRight className='absolute text-blue-900 translate-x-[-18px]  translate-y-1' title='today' />} {e.getDate()} {month[e.getMonth()]}</h3>
+                <p className='text-sm text-gray-400'>{week[e.getDay()]}</p>
+            </div>
+            {/* events list */}
+                {
+                    title[i]!=""?(
+                        <div className='py-1 px-5 shadow-lg rounded-full w-full  text-center bg-blue-300'>
+                            <h3>
+                               {title[i]}
+                            </h3>
+                            <p className='text-sm text-gray-500'>10:00 am</p>
+
+
+                        </div>
+                    ):(<div  className='py-1 px-5 shadow-lg rounded-full w-full  text-center bg-gray-300'>
+                        <h3>
+                          No Event
+                        </h3>
+                    </div>)
+                }
+            </div>
+            ))
+        }
+    </div>
+  )
+}
+
+export default Calender
