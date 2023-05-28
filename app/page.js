c0ae6377c9ddd9d3372846a78ce0d95063ee10aa
prafuljain123 <prafuljain303@gmail.com>
2023-05-28 15:53:47 +0530

    progress component added

--- app/page.js
+++ app/page.js
@@ -2,12 +2,18 @@ import {assignment} from '@/Data/Data'
 import Assignment from '@/components/Assignment'
 import Calender from '@/components/Calender'
 import Card1 from '@/components/Card1'
-
+import Progress from '@/components/progress'
 
 export default function Home() {
   return (
     <div className='center h-screen w- screen'>
-      <Calender/>
+      {/* <Calender/> */}
+      <Progress />
     </div>
   )
 }
+
+
+
+
+
