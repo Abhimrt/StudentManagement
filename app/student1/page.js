bb5890e443f1f2068da9f3781f823d3067221d72
prafuljain123 <prafuljain303@gmail.com>
2023-06-07 14:58:24 +0530

    new

---
+++ app/student1/page.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './student.css'
+
+const page = () => {
+  return (
+    <div className='container'>
+        <div className='item item1'>
+
+        </div>
+
+        <div className='item item2'>
+            
+        </div>
+
+        <div className='item item3'>
+            
+        </div>
+
+        <div className='item item4'>
+            
+        </div>
+
+        <div className='item item5'>
+            
+        </div>
+
+        <div className='item item6'>
+            
+        </div>
+
+        <div className='item item7'>
+            
+        </div>
+
+        <div className='item item8'>
+            
+        </div>
+
+        <div className='item item9'>
+            
+        </div>
+
+        <div className='item item10'>
+            
+        </div>
+
+        <div className='item item11'>
+            
+        </div>
+
+        <div className='item item12'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default page
