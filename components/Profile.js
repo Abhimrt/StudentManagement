4ea20f5bfac35bdc550acb22b43e7ef5fdcca9d1
Abhishek Singhal <86764280+Abhimrt@users.noreply.github.com>
2023-06-07 19:32:38 +0530

    profile working

--- components/Profile.js
+++ components/Profile.js
@@ -7,13 +7,13 @@ import { RiContactsFill } from "react-icons/ri"
 import Link from "next/link";
 
 const Profile = ({ data }) => {
-
-  const [active, setactive] = useState("")
   const profile = useRef()
   const contact = useRef()
   const address = useRef()
+  const content = useRef()
 
   const navBar = (e) => {
+    console.log(e.current.innerText)
     if (!e.current.classList.contains("active")) {
       profile.current.classList.remove("active")
       profile.current.classList.add("nactive")
@@ -23,122 +23,137 @@ const Profile = ({ data }) => {
       address.current.classList.add("nactive")
       e.current.classList.remove("nactive")
       e.current.classList.add("active")
+      if(e.current.innerText == "Profile"){
+        content.current.style.transform=`translateX(0)`
+      }else if(e.current.innerText == "Contact"){
+        content.current.style.transform=`translateX(-33.3%)`
+      }else{
+        content.current.style.transform=`translateX(-66.6%)`
+      }
     }
   }
 
   return (
-    <div className="w-full self-stretch p-0  text-center profile" >
+    <div className="w-full self-stretch p-0  text-center profile overflow-hidden" >
       <div className="  bg-slate-200 border h-full border-gray-200 rounded-lg shadow-lg">
 
         {/* upper navbar start */}
         <div className="border-b border-slate-300 ">
           <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 ">
             <li className="mr-2 active" ref={profile}>
-              <Link href="/student" className=" group " onClick={() => navBar(profile)}>
+              <div  className=" group " onClick={() => navBar(profile)}>
                 <CgProfile />Profile
-              </Link>
+              </div>
             </li>
             <li className="mr-2 nactive" ref={contact} >
-              <Link href="/student" className=" group" onClick={() => navBar(contact)} >
+              <div  className=" group" onClick={() => navBar(contact)} >
                 <RiContactsFill />Contact
-              </Link>
+              </div>
             </li>
             <li className="mr-2 nactive" ref={address} >
-              <Link href="/student" className="  group" onClick={() => navBar(address)}>
+              <div  className="  group" onClick={() => navBar(address)}>
                 <MdLocationOn />address
-              </Link>
+              </div>
             </li>
           </ul>
         </div>
         {/* upper navbar end */}
+        <div className="contentProfile center" ref={content}>
 
-        {/* profile section start */}
-        <div className="hidden">
-          <Image src={"/user.png"} width={200} height={200} className="m-auto " />
-          <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 capitalize">
-            {data.studentName}
-          </h2>
+          {/* profile section start */}
+          <div className=" w-full ">
+          <div className="img">
+              <Image src={"/user.jpeg"} width={200} height={200} className="h-full" />
+            </div>
+            <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 capitalize">
+              {data.studentName}
+            </h2>
 
-          <p className="mb-3 font-normal text-gray-500 ">
-            <span className="text-gray-700">Session</span> : {data.AddmissionSession}
-          </p>
-          <p className="mb-3 font-normal text-gray-500 ">
-            <span className="text-gray-700" >Course</span> : {data.Course}
-            <span className="ml-5"><span className="text-gray-700">Batch</span> : {data.batch}</span>
-          </p>
-          <p className=" mb-3 font-normal text-gray-500 space-x-3 ">
-            <span><span className="text-gray-700">Father Name</span> : {data.perents.FatherName}</span>
-            <br className="block md:hidden" />
-            <span > <span className="text-gray-700 ">Mob. No.</span> : {data.perents.FatherMobileNumber}</span>
-          </p>
-          <p className="mb-3 font-normal text-gray-5d00">
-            <span className="text-gray-700 text-left ">Address</span> <br />
-            <span className="text-gray-700">House No.</span> : {data.Address.HouseNo}
-            <span className="ml-5"> <span className="text-gray-700">Street</span> : {data.Address.street}</span>
-            <br className="block md:hidden" />
-            <span className="ml-5"> <span className="text-gray-700">Area</span> : {data.Address.area}</span>
-          </p>
+            <p className="mb-3 font-normal text-gray-500 ">
+              <span className="text-gray-700">Session</span> : {data.AddmissionSession}
+            </p>
+            <p className="mb-3 font-normal text-gray-500 ">
+              <span className="text-gray-700" >Course</span> : {data.Course}
+              <span className="ml-5"><span className="text-gray-700">Batch</span> : {data.batch}</span>
+            </p>
+            <p className=" mb-3 font-normal text-gray-500 space-x-3 ">
+              <span><span className="text-gray-700">Father Name</span> : {data.perents.FatherName}</span>
+              <br className="block md:hidden" />
+              <span > <span className="text-gray-700 ">Mob. No.</span> : {data.perents.FatherMobileNumber}</span>
+            </p>
+            <p className="mb-3 font-normal text-gray-5d00">
+              <span className="text-gray-700 text-left ">Address</span> <br />
+              <span className="text-gray-700">House No.</span> : {data.Address.HouseNo}
+              <span className="ml-5"> <span className="text-gray-700">Street</span> : {data.Address.street}</span>
+              <br className="block md:hidden" />
+              <span className="ml-5"> <span className="text-gray-700">Area</span> : {data.Address.area}</span>
+            </p>
+          </div>
+          {/* profile section end */}
+
+          {/* contact section start */}
+          <div className="text-left w-full ">
+            <div className="img">
+              <Image src={"/user.jpeg"} width={200} height={200} className="h-full" />
+            </div>
+            <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 capitalize">
+              {data.studentName}
+            </h2>
+
+            <p className="mb-3 font-normal text-gray-500 ">
+              <span className="text-gray-700">Session</span> : {data.AddmissionSession}
+            </p>
+            <p className="mb-3 font-normal text-gray-500 ">
+              <span className="text-gray-700" >Course</span> : {data.Course}
+              <span className="ml-5"><span className="text-gray-700">Batch</span> : {data.batch}</span>
+            </p>
+            <p className=" mb-3 font-normal text-gray-500 space-x-3 ">
+              <span><span className="text-gray-700">Father Name</span> : {data.perents.FatherName}</span>
+
+              <span > <span className="text-gray-700 ">Mob. No.</span> : {data.perents.FatherMobileNumber}</span>
+            </p>
+            <p className="mb-3 font-normal text-gray-500">
+              <span className="text-gray-700 text-left ">Address</span> <br />
+              <span className="text-gray-700">House No.</span> : {data.Address.HouseNo}
+              <span className="ml-5"> <span className="text-gray-700">Street</span> : {data.Address.street}</span>
+
+              <span className="ml-5"> <span className="text-gray-700">Area</span> : {data.Address.area}</span>
+            </p>
+          </div>
+          {/* contact section end */}
+
+          {/* address section start */}
+          <div className="w-full" >
+          <div className="img">
+              <Image src={"/user.jpeg"} width={200} height={200} className="h-full" />
+            </div>
+            <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 capitalize">
+              Address
+            </h2>
+
+            <p className="mb-3 font-normal text-gray-500 ">
+              <span className="text-gray-700">Session</span> : {data.AddmissionSession}
+            </p>
+            <p className="mb-3 font-normal text-gray-500 ">
+              <span className="text-gray-700" >Course</span> : {data.Course}
+              <span className="ml-5"><span className="text-gray-700">Batch</span> : {data.batch}</span>
+            </p>
+            <p className=" mb-3 font-normal text-gray-500 space-x-3 ">
+              <span><span className="text-gray-700">Father Name</span> : {data.perents.FatherName}</span>
+              <br className="block md:hidden" />
+              <span > <span className="text-gray-700 ">Mob. No.</span> : {data.perents.FatherMobileNumber}</span>
+            </p>
+            <p className="mb-3 font-normal text-gray-5d00">
+              <span className="text-gray-700 text-left ">Address</span> <br />
+              <span className="text-gray-700">House No.</span> : {data.Address.HouseNo}
+              <span className="ml-5"> <span className="text-gray-700">Street</span> : {data.Address.street}</span>
+              <br className="block md:hidden" />
+              <span className="ml-5"> <span className="text-gray-700">Area</span> : {data.Address.area}</span>
+            </p>
+          </div>
+          {/* address section end */}
+
         </div>
-        {/* profile section end */}
-
-        {/* contact section start */}
-        <div className="text-left pl-5">
-          <Image src={"/user.png"} width={200} height={200} className="m-auto " />
-          <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 capitalize">
-            {data.studentName}
-          </h2>
-
-          <p className="mb-3 font-normal text-gray-500 ">
-            <span className="text-gray-700">Session</span> : {data.AddmissionSession}
-          </p>
-          <p className="mb-3 font-normal text-gray-500 ">
-            <span className="text-gray-700" >Course</span> : {data.Course}
-            <span className="ml-5"><span className="text-gray-700">Batch</span> : {data.batch}</span>
-          </p>
-          <p className=" mb-3 font-normal text-gray-500 space-x-3 ">
-            <span><span className="text-gray-700">Father Name</span> : {data.perents.FatherName}</span>
-           
-            <span > <span className="text-gray-700 ">Mob. No.</span> : {data.perents.FatherMobileNumber}</span>
-          </p>
-          <p className="mb-3 font-normal text-gray-500">
-            <span className="text-gray-700 text-left ">Address</span> <br />
-            <span className="text-gray-700">House No.</span> : {data.Address.HouseNo}
-            <span className="ml-5"> <span className="text-gray-700">Street</span> : {data.Address.street}</span>
-    
-            <span className="ml-5"> <span className="text-gray-700">Area</span> : {data.Address.area}</span>
-          </p>
-        </div>
-        {/* contact section end */}
-
-        {/* address section start */}
-        <div className="hidden">
-          <Image src={"/user.png"} width={200} height={200} className="m-auto " />
-          <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 capitalize">
-            {data.studentName}
-          </h2>
-
-          <p className="mb-3 font-normal text-gray-500 ">
-            <span className="text-gray-700">Session</span> : {data.AddmissionSession}
-          </p>
-          <p className="mb-3 font-normal text-gray-500 ">
-            <span className="text-gray-700" >Course</span> : {data.Course}
-            <span className="ml-5"><span className="text-gray-700">Batch</span> : {data.batch}</span>
-          </p>
-          <p className=" mb-3 font-normal text-gray-500 space-x-3 ">
-            <span><span className="text-gray-700">Father Name</span> : {data.perents.FatherName}</span>
-            <br className="block md:hidden" />
-            <span > <span className="text-gray-700 ">Mob. No.</span> : {data.perents.FatherMobileNumber}</span>
-          </p>
-          <p className="mb-3 font-normal text-gray-5d00">
-            <span className="text-gray-700 text-left ">Address</span> <br />
-            <span className="text-gray-700">House No.</span> : {data.Address.HouseNo}
-            <span className="ml-5"> <span className="text-gray-700">Street</span> : {data.Address.street}</span>
-            <br className="block md:hidden" />
-            <span className="ml-5"> <span className="text-gray-700">Area</span> : {data.Address.area}</span>
-          </p>
-        </div>
-        {/* address section end */}
-
 
       </div>
     </div>
